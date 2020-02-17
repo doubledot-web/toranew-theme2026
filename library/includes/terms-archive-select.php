171c2edae942948ec877a8f20f76428dcfcc113b
Dimitris <dimitris@wearedoubledot.com>
2020-02-17 16:55:12 +0200

    Added label below terms version selection

--- library/includes/terms-archive-select.php
+++ library/includes/terms-archive-select.php
@@ -3,15 +3,23 @@ $terms_versions = get_field( 'versions' );
 
 if ( $terms_versions ) : ?>
 	<div class="terms-archive">
-		<select>
-			<option value="" disabled selected>Αρχείο</option>
-			<?php
-			foreach ( $terms_versions as $version ) :
-				$version = $version['version'] ?>
-				<option value="<?php esc_attr_e( $version['file'] ); ?>"><?php esc_html_e( $version['date'] ); ?></option>
-			<?php endforeach; ?>
-		</select>
-	</div>
+		<div>
+			<select>
+				<option value="" disabled selected><?php _e( 'Επιλέξτε έκδοση / Select version', 'tora' ); ?></option>
+				<?php
+				foreach ( $terms_versions as $version ) :
+					$version = $version['version'] ?>
+					<option value="<?php esc_attr_e( $version['file'] ); ?>"><?php esc_html_e( $version['date'] ); ?></option>
+				<?php endforeach; ?>
+			</select>
 
+			<?php
+			$version_label = get_field( 'version_label' );
+
+			if ( $version_label ) : ?>
+				<p><?php esc_html_e( $version_label ); ?></p>
+			<?php endif; ?>
+		</div>
+	</div>
 <?php
 endif; ?>
