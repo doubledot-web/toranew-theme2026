f1a4e67d036d18097ca866584f528413dfd3eaa9
Panayotis <panayotis.kalogirou@gmail.com>
2026-01-19 10:04:38 +0200

    change map icon - fix

--- legacy/functions.php
+++ legacy/functions.php
@@ -188,7 +188,7 @@ class Theme
 			'locations_url'        => get_rest_url( null, '/posts/locations' ),
 			'map_link_text'        => __( 'Get directions', TORA_B2C_TEXTDOMAIN ),
 			'map_tel_text'         => __( 'Tel', TORA_B2C_TEXTDOMAIN ),
-			'opap_store_icon'	   => get_stylesheet_directory_uri() .'/images/allwyn-store-poi-small.png',
+			'opap_store_icon'	   => get_stylesheet_directory_uri() .'/images/allwyn-store-poi-small2.png',
 			'other_store_icon'	   => get_stylesheet_directory_uri() .'/images/retail-store-small.png',
 			'zoom_out_of_greece'   => $options['locations_map_zoom_out_of_greece'],
 		);
